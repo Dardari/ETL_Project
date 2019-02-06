f66bc253a6527cb4cc6d46d1a9ebb73769e4f227
Pablo Maceda <pablomaceda@Pablos-MacBook-Pro.local>
2019-02-05 20:04:27 -0600

    final final commit

--- etl_project.sql
+++ etl_project.sql
@@ -26,15 +26,14 @@ CREATE TABLE imr (
     number_of_births TEXT
 );
 
-CREATE DATABASE restaurant_scores_db;
-USE restaurant_scores_db;
 
 CREATE TABLE restaurant_scores (
-facility_id INT PRIMARY KEY,
-restaurant_name TEXT,
-zip_code INT,
-inspect_date TEXT,
-inspect_score INT
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	facility_id INT,
+	restaurant_name TEXT,
+	zip_code INT,
+	inspect_date TEXT,
+	inspect_score INT
 );
 
 CREATE TABLE crime_data (
@@ -42,5 +41,29 @@ CREATE TABLE crime_data (
     zipcode INT,
     count INT
 );
+
+SELECT * FROM parks;
+SELECT * FROM restaurant_scores;
+
+SELECT infant_mortality_rate, count, zip_code
+FROM crime_data
+INNER JOIN
+ON crime_data.zipcode = imr.zip_code;
+
+SELECT r_z.average AS AVG_REST_INSPECT_SCORE, p_z.cnt AS PARK_COUNT, p_z.zip_code
+FROM (SELECT AVG(inspect_score) as average, zip_code
+		FROM restaurant_scores
+        GROUP BY zip_code) r_z
+INNER JOIN (SELECT count(zip_code) as cnt, zip_code
+			FROM parks
+            GROUP BY zip_code) p_z
+ON p_z.zip_code = r_z.zip_code;
+
+SELECT p_z.cnt AS PARK_COUNT, count AS CRIME_COUNT, zipcode
+FROM crime_data c_d
+INNER JOIN (SELECT COUNT(zip_code) as cnt, zip_code
+			FROM parks
+            GROUP BY zip_code) p_z
+ON p_z.zip_code = c_d.zipcode;
 
 
